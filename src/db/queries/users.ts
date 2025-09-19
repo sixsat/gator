8a51c01cd07f0559be7209e1536ac74ec98d7bd1
sat <77387934+sixsat@users.noreply.github.com>
2025-09-19 22:50:11 +0700

    add queries

---
+++ src/db/queries/users.ts
@@ -0,0 +1,22 @@
+import { db } from "..";
+import { users } from "../schema";
+import { eq } from "drizzle-orm";
+import { firstOrUndefined } from "./utils";
+
+export async function createUser(name: string) {
+  const [result] = await db.insert(users).values({ name: name }).returning();
+  return result;
+}
+
+export async function getUser(name: string) {
+  const result = await db.select().from(users).where(eq(users.name, name));
+  return firstOrUndefined(result);
+}
+
+export async function deleteUsers() {
+  await db.delete(users);
+}
+
+export async function getUsers() {
+  return db.select().from(users);
+}
