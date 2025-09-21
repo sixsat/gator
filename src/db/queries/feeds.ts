190de3df5735a09770886029d907c195b182c3c8
sat <77387934+sixsat@users.noreply.github.com>
2025-09-21 13:50:31 +0700

    add feeds and feed_follows

---
+++ src/db/queries/feeds.ts
@@ -0,0 +1,86 @@
+import { eq, and } from "drizzle-orm";
+import { db } from "..";
+import { feeds, feedFollows, users } from "../schema";
+import { firstOrUndefined } from "./utils";
+
+export async function createFeed(
+  feedName: string,
+  url: string,
+  userId: string
+) {
+  const result = await db
+    .insert(feeds)
+    .values({
+      name: feedName,
+      url,
+      userId,
+    })
+    .returning();
+
+  return firstOrUndefined(result);
+}
+
+export async function getFeeds() {
+  const result = await db.select().from(feeds);
+  return result;
+}
+
+export async function getFeedByURL(url: string) {
+  const result = await db.select().from(feeds).where(eq(feeds.url, url));
+  return firstOrUndefined(result);
+}
+
+export async function createFeedFollow(userId: string, feedId: string) {
+  const [newFeedFollow] = await db
+    .insert(feedFollows)
+    .values({ feedId, userId })
+    .returning();
+
+  const [result] = await db
+    .select({
+      id: feedFollows.id,
+      createdAt: feedFollows.createdAt,
+      updatedAT: feedFollows.updatedAt,
+      userId: feedFollows.userId,
+      feedId: feedFollows.feedId,
+      feedName: feeds.name,
+      userName: users.name,
+    })
+    .from(feedFollows)
+    .innerJoin(feeds, eq(feedFollows.feedId, feeds.id))
+    .innerJoin(users, eq(feedFollows.userId, users.id))
+    .where(
+      and(
+        eq(feedFollows.id, newFeedFollow.id),
+        eq(users.id, newFeedFollow.userId)
+      )
+    );
+
+  return result;
+}
+
+export async function getFeedFollowsForUser(userId: string) {
+  const result = await db
+    .select({
+      id: feedFollows.id,
+      createdAt: feedFollows.createdAt,
+      updatedAT: feedFollows.updatedAt,
+      userId: feedFollows.userId,
+      feedId: feedFollows.feedId,
+      feedname: feeds.name,
+    })
+    .from(feedFollows)
+    .innerJoin(feeds, eq(feedFollows.feedId, feeds.id))
+    .where(eq(feedFollows.userId, userId));
+
+  return result;
+}
+
+export async function deleteFeedFollow(feedId: string, userId: string) {
+  const [result] = await db
+    .delete(feedFollows)
+    .where(and(eq(feedFollows.feedId, feedId), eq(feedFollows.userId, userId)))
+    .returning();
+
+  return result;
+}
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, timestamp, uuid, text } from "drizzle-orm/pg-core";
+import { pgTable, timestamp, uuid, text, unique } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
   id: uuid("id").primaryKey().defaultRandom(),
@@ -9,3 +9,42 @@ export const users = pgTable("users", {
     .$onUpdate(() => new Date()),
   name: text("name").notNull().unique(),
 });
+
+export type User = typeof users.$inferSelect;
+
+export const feeds = pgTable("feeds", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+  name: text("name").notNull(),
+  url: text("url").notNull().unique(),
+  userId: uuid("user_id")
+    .references(() => users.id, { onDelete: "cascade" })
+    .notNull(),
+});
+
+export type Feed = typeof feeds.$inferSelect;
+
+export const feedFollows = pgTable(
+  "feed_follows",
+  {
+    id: uuid("id").primaryKey().defaultRandom().notNull(),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    updatedAt: timestamp("updated_at")
+      .notNull()
+      .defaultNow()
+      .$onUpdate(() => new Date()),
+    userId: uuid("user_id")
+      .references(() => users.id, { onDelete: "cascade" })
+      .notNull(),
+    feedId: uuid("feed_id")
+      .notNull()
+      .references(() => feeds.id, { onDelete: "cascade" }),
+  },
+  (t) => [unique().on(t.userId, t.feedId)]
+);
+
+export type FeedFollow = typeof feedFollows.$inferSelect;
